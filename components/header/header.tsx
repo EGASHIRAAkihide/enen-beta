fd4d8ccf99cad2fe8994316d2449f4c55f7c7c03
akihide.egashira <42921740+EGASHIRAAkihide@users.noreply.github.com>
2022-06-20 20:26:30 +0900

    (feat) [issue-001] pcでのheader表示

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -9,7 +9,8 @@ const Header = () => {
 
   return (
     <header className="bg-slate-300 relative">
-      <div className="flex flex-nowrap items-center justify-between px-5">
+      <div className="
+        flex flex-nowrap items-center justify-between mx-auto my-0 px-5 md:px-8 lg:px-12 max-w-5xl">
         <div>
           <Image
             src="/logo.svg"
@@ -18,42 +19,76 @@ const Header = () => {
             height="100%"
           />
         </div>
-        <div className="animate-bounce cursor-pointer flex items-center w-8" onClick={() => setIsNavOpen(!isNavOpen)}>
+        <nav className="hidden lg:block">
+          <ul className="flex gap-10 items-center">
+            <li className="cursor-pointer">
+              <a className="text-base text-slate-800">About</a>
+            </li>
+            <li className="cursor-pointer">
+              <a className="text-base text-slate-800">Company</a>
+            </li>
+            <li className="cursor-pointer">
+              <a className="text-base text-slate-800">Works</a>
+            </li>
+            <li className="cursor-pointer">
+              <a className="text-base text-slate-800">Members</a>
+            </li>
+            <li className="cursor-pointer">
+              <a className="text-base text-slate-800">Contact</a>
+            </li>
+            <li className="cursor-pointer">
+              <p>Related Business</p>
+              <ul className="hidden">
+                <li>
+                  <a className="text-base text-slate-800">ENcounter ENgravers</a>
+                </li>
+                <li>
+                  <a className="text-base text-slate-800">ENART</a>
+                </li>
+                <li>
+                  <a className="text-base text-slate-800">ENIT</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="animate-bounce cursor-pointer flex items-center w-8 lg:hidden" onClick={() => setIsNavOpen(!isNavOpen)}>
           <GiHamburger size={32} />
         </div>
       </div>
-      <nav className={isNavOpen ? "absolute bg-white flex flex-wrap items-center jusfity-start py-8 w-full" : "hidden"}>
+      <nav className={isNavOpen ? "absolute bg-white flex flex-wrap items-center jusfity-start py-8 w-full lg:hidden" : "hidden"}>
         <ul className="w-full">
-          <li className="cursor-pointer pt-3 pr-3 pb-3 pl-6 transition-all w-full hover:bg-slate-100">
-            <a href="#about" className="text-md">About ENEN</a>
+          <li className="cursor-pointer pt-3 md:pt-6 pr-3 md:pr-6 pb-3 md:pb-6 pl-6 md:pl-11 transition-all w-full hover:bg-slate-100">
+            <a href="#about" className="text-base md:text-lg">About ENEN</a>
           </li>
-          <li className="cursor-pointer pt-3 pr-3 pb-3 pl-6 transition-all w-full hover:bg-slate-100">
-            <a href="#company" className="text-md">Company</a>
+          <li className="cursor-pointer pt-3 md:pt-6 pr-3 md:pr-6 pb-3 md:pb-6 pl-6 md:pl-11 transition-all w-full hover:bg-slate-100">
+            <a href="#company" className="text-base md:text-lg">Company</a>
           </li>
-          <li className="cursor-pointer pt-3 pr-3 pb-3 pl-6 transition-all w-full hover:bg-slate-100">
-            <a href="#work" className="text-md">Work</a>
+          <li className="cursor-pointer pt-3 md:pt-6 pr-3 md:pr-6 pb-3 md:pb-6 pl-6 md:pl-11 transition-all w-full hover:bg-slate-100">
+            <a href="#work" className="text-base md:text-lg">Works</a>
           </li>
-          <li className="cursor-pointer pt-3 pr-3 pb-3 pl-6 transition-all w-full hover:bg-slate-100">
-            <a href="#members" className="text-md">Members</a>
+          <li className="cursor-pointer pt-3 md:pt-6 pr-3 md:pr-6 pb-3 md:pb-6 pl-6 md:pl-11 transition-all w-full hover:bg-slate-100">
+            <a href="#members" className="text-base md:text-lg">Members</a>
           </li>
-          <li className="cursor-pointer pt-3 pr-3 pb-3 pl-6 transition-all w-full hover:bg-slate-100">
-            <a href="#contact" className="text-md">Contact</a>
+          <li className="cursor-pointer pt-3 md:pt-6 pr-3 md:pr-6 pb-3 md:pb-6 pl-6 md:pl-11 transition-all w-full hover:bg-slate-100">
+            <a href="#contact" className="text-base md:text-lg">Contact</a>
           </li>
-          <li className="cursor-pointer pt-3 pr-3 pb-3 pl-6 transition-all w-full hover:bg-slate-100">
-            Related Business
-            <ul className="pt-4 pl-4">
-              <li className="pt-2 pr-2 pb-2 pl-2">
-                <a className="text-sm">ENcounter ENgravers</a>
+          <li className="pt-3 md:pt-6 pr-3 md:pr-6 pb-3 md:pb-6 pl-6 md:pl-11">
+            <p className="text-base md:text-lg">Related Business</p>
+            <ul className="pt-4 md:pt-6 pl-4">
+              <li className="cursor-pointer pt-2 md:pt-4 pr-2 md:pr-4 pb-2 md:pb-4 pl-2 md:pl-4 transition-all w-full hover:bg-slate-100">
+                <a className="text-sm md:text-base">ENcounter ENgravers</a>
               </li>
-              <li className="pt-2 pr-2 pb-2 pl-2">
-                <a className="text-sm">ENART</a>
+              <li className="cursor-pointer pt-2 md:pt-4 pr-2 md:pr-4 pb-2 md:pb-4 pl-2 md:pl-4 transition-all w-full hover:bg-slate-100">
+                <a className="text-sm md:text-base">ENART</a>
               </li>
-              <li className="pt-2 pr-2 pb-2 pl-2">
-                <a className="text-sm">ENIT</a>
+              <li className="cursor-pointer pt-2 md:pt-4 pr-2 md:pr-4 pb-2 md:pb-4 pl-2 md:pl-4 transition-all w-full hover:bg-slate-100">
+                <a className="text-sm md:text-base">ENIT</a>
               </li>
             </ul>
           </li>
-          <li className="cursor-pointer pt-3 pr-8 pb-3 pl-3 flex gap-6 items-center justify-end">
+          <li className="cursor-pointer pt-3 md:pt-6 pr-8 md:pr-11 pb-3 md:pb-6 pl-3 md:pl-6 flex gap-6 items-center justify-end">
             <a href="https://www.instagram.com/encounter_engravers/" target="_blank" rel="noreferrer">
               <AiOutlineInstagram size={24} />
             </a>
