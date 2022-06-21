58f0872fbdcd368c8cb6a54fa7617e6942cb4bc3
akihide.egashira <42921740+EGASHIRAAkihide@users.noreply.github.com>
2022-06-21 14:58:36 +0900

    (feat) [issue-001] splashを追加

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -37,18 +37,7 @@ const Header = () => {
               <a className="text-base text-slate-800">Contact</a>
             </li>
             <li className="cursor-pointer">
-              <p>Related Business</p>
-              <ul className="hidden">
-                <li>
-                  <a className="text-base text-slate-800">ENcounter ENgravers</a>
-                </li>
-                <li>
-                  <a className="text-base text-slate-800">ENART</a>
-                </li>
-                <li>
-                  <a className="text-base text-slate-800">ENIT</a>
-                </li>
-              </ul>
+              <a className="text-base text-slate-800">Group Companies</a>
             </li>
           </ul>
         </nav>
@@ -75,18 +64,7 @@ const Header = () => {
             <a href="#contact" className="text-base md:text-lg">Contact</a>
           </li>
           <li className="pt-3 md:pt-6 pr-3 md:pr-6 pb-3 md:pb-6 pl-6 md:pl-11">
-            <p className="text-base md:text-lg">Related Business</p>
-            <ul className="pt-4 md:pt-6 pl-4">
-              <li className="cursor-pointer pt-2 md:pt-4 pr-2 md:pr-4 pb-2 md:pb-4 pl-2 md:pl-4 transition-all w-full hover:bg-slate-100">
-                <a className="text-sm md:text-base">ENcounter ENgravers</a>
-              </li>
-              <li className="cursor-pointer pt-2 md:pt-4 pr-2 md:pr-4 pb-2 md:pb-4 pl-2 md:pl-4 transition-all w-full hover:bg-slate-100">
-                <a className="text-sm md:text-base">ENART</a>
-              </li>
-              <li className="cursor-pointer pt-2 md:pt-4 pr-2 md:pr-4 pb-2 md:pb-4 pl-2 md:pl-4 transition-all w-full hover:bg-slate-100">
-                <a className="text-sm md:text-base">ENIT</a>
-              </li>
-            </ul>
+            <a href="#contact" className="text-base md:text-lg">Group Companies</a>
           </li>
           <li className="cursor-pointer pt-3 md:pt-6 pr-8 md:pr-11 pb-3 md:pb-6 pl-3 md:pl-6 flex gap-6 items-center justify-end">
             <a href="https://www.instagram.com/encounter_engravers/" target="_blank" rel="noreferrer">
@@ -98,8 +76,8 @@ const Header = () => {
           </li>
         </ul>
       </nav>
-      <div className="hidden">
-        hero image
+      <div>
+        hero
       </div>
     </header>
   )
